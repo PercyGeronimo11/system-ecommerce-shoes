304c51e623e137355406b8b74717b2837eb781fa
PercyGeronimo <percy.geronimo.d@gmail.com>
2024-05-31 01:26:40 -0500

    first commit the percydev

--- src/main/java/com/hb/system/ecommerce/shoes/SystemEcommerceShoesAppJavaApplication.java
+++ src/main/java/com/hb/system/ecommerce/shoes/SystemEcommerceShoesAppJavaApplication.java
@@ -9,5 +9,4 @@ public class SystemEcommerceShoesAppJavaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SystemEcommerceShoesAppJavaApplication.class, args);
 	}
-
 }
