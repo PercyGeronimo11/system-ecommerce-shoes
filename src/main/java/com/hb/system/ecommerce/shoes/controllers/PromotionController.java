058e12cd5769f7ac39b9fb56290799f952da8f8f
CRISTHIAN390 <70170324+CRISTHIAN390@users.noreply.github.com>
2024-06-19 02:55:42 -0500

    Actualizad

--- src/main/java/com/hb/system/ecommerce/shoes/controllers/PromotionController.java
+++ src/main/java/com/hb/system/ecommerce/shoes/controllers/PromotionController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.hb.system.ecommerce.shoes.entity.Category;
 import com.hb.system.ecommerce.shoes.entity.Promotion;
 import com.hb.system.ecommerce.shoes.repositories.PromotionRepository;
 
