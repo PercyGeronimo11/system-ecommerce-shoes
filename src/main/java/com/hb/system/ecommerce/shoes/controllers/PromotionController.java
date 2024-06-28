d5649cd1b6237a33bfe17806fd2ba5dcdeb867a5
CRISTHIAN390 <70170324+CRISTHIAN390@users.noreply.github.com>
2024-06-28 12:38:50 -0500

    Actualizad

--- src/main/java/com/hb/system/ecommerce/shoes/controllers/PromotionController.java
+++ src/main/java/com/hb/system/ecommerce/shoes/controllers/PromotionController.java
@@ -4,14 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.hb.system.ecommerce.shoes.entity.Promotion;
 import com.hb.system.ecommerce.shoes.services.PromotionService;
@@ -30,13 +31,6 @@ public class PromotionController {
         return promotionService.getAllPromotions();
     }
 
-    @GetMapping({ "/create" })
-    public String getCreatePromotion(Promotion promocion, Model model) {
-        model.addAttribute("promotion", promocion);
-        model.addAttribute("contenido", "promotions/PromotionCreate");
-        return "layout/index";
-    }
-
     @PostMapping({ "/new" })
     public ResponseEntity<String> createPromotion(@RequestBody Promotion promotion) {
         try {
@@ -49,34 +43,13 @@ public class PromotionController {
         }
     }
 
-
-
-
-    @GetMapping("delete/{id}")
-    public String eliminarPromotion(@PathVariable int id, RedirectAttributes redirectAttributes) {
-        Promotion promotion = promotionRepository.findById(id);
-        promotionRepository.delete(promotion);
-        return "redirect:/promotion/list";
+    @PatchMapping("/{id}")
+    public void UpdatePromotion(@PathVariable Integer id, @RequestBody Promotion promotion) {
+        promotionService.updatePromotion(id, promotion);
     }
 
-    @GetMapping("edit/{id}")
-    public String getEditPromotion(@PathVariable int id, Model model) {
-        Promotion promotion = promotionRepository.findById(id);
-        model.addAttribute("promotion", promotion);
-        model.addAttribute("contenido", "promotions/PromotionEdit");
-        return "layout/index";
-    }
-
-    @PostMapping("update/{id}")
-    public String UpdatePromotion(@PathVariable int id, @ModelAttribute Promotion promotion, Model model) {
-        Promotion promotionOld = promotionRepository.findById(id);
-        promotionOld = promotion;
-        promotionOld.setPROMT_id(id);
-        promotionRepository.save(promotionOld);
-        return "redirect:/promotion/list";
-    }
-    @GetMapping("/cancel")
-    public String cancel(){
-        return "redirect:/promotion/list";
+    @DeleteMapping("/{id}")
+    public void deletePromotion(@PathVariable Integer id) {
+        promotionService.deletePromotion(id);
     }
 }
