1b1855907e3f9889074c074d5807f69c376d6ca1
PercyGeronimo <percy.geronimo.d@gmail.com>
2024-06-30 18:13:53 -0500

    ss

--- src/main/java/com/hb/system/ecommerce/shoes/SystemEcommerceShoesAppJavaApplication.java
+++ src/main/java/com/hb/system/ecommerce/shoes/SystemEcommerceShoesAppJavaApplication.java
@@ -2,10 +2,12 @@ package com.hb.system.ecommerce.shoes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+//@SpringBootApplication
 @ComponentScan(basePackages = "com.hb.system.ecommerce.shoes")
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class SystemEcommerceShoesAppJavaApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/hb/system/ecommerce/shoes/config/SecurityConfig.java
+++ src/main/java/com/hb/system/ecommerce/shoes/config/SecurityConfig.java
@@ -6,16 +6,16 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig {
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .requestMatchers("/**").permitAll() // Permitir todas las rutas sin autenticación
-                .and().csrf().disable(); // Deshabilitar CSRF para permitir peticiones POST
-
-        return http.build();
-    }
-}
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .requestMatchers("/**").permitAll() // Permitir todas las rutas sin autenticación
+//                //.and().csrf().disable(); // Deshabilitar CSRF para permitir peticiones POST
+//
+//        return http.build();
+//    }
+//}
