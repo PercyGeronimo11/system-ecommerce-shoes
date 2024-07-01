9c0e48a61cdf36bfeb7beaaba9d175b66a9b40fd
Jack Portilla <jackportilla24@gmail.com>
2024-07-01 01:41:28 -0500

    .

--- src/main/java/com/hb/system/ecommerce/shoes/entity/Product.java
+++ src/main/java/com/hb/system/ecommerce/shoes/entity/Product.java
@@ -1,9 +1,11 @@
 package com.hb.system.ecommerce.shoes.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Data
 @Entity
