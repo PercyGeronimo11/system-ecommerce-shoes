afa5633c0e3fd16864278cb3606ac6810a797b5e
PercyGeronimo <percy.geronimo.d@gmail.com>
2024-07-01 01:21:43 -0500

    corregido product con image

--- src/main/java/com/hb/system/ecommerce/shoes/SystemEcommerceShoesAppJavaApplication.java
+++ src/main/java/com/hb/system/ecommerce/shoes/SystemEcommerceShoesAppJavaApplication.java
@@ -2,6 +2,8 @@ package com.hb.system.ecommerce.shoes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 //<<<<<<< HEAD
 //import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 //import org.springframework.context.annotation.ComponentScan;
@@ -10,7 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //@ComponentScan(basePackages = "com.hb.system.ecommerce.shoes")
 //@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 //=======
-
+@ComponentScan(basePackages = "com.hb.system.ecommerce.shoes")
 @SpringBootApplication
 public class SystemEcommerceShoesAppJavaApplication {
 
--- src/main/java/com/hb/system/ecommerce/shoes/entity/Product.java
+++ src/main/java/com/hb/system/ecommerce/shoes/entity/Product.java
@@ -18,8 +18,18 @@ public class Product {
     private String proDescription;
     @Column(name = "pro_unit_price")
     private String proUnitPrice;
+    @Column(name = "pro_size")
+    private String proSize;
     @Column(name = "pro_size_platform")
     private String proSizePlatform;
     @Column(name = "pro_size_tacon")
     private String proSizeTacon;
+    @Column(name = "pro_url_image")
+    private String proUrlImage;
+    @Column(name = "pro_color")
+    private String proColor;
+    @Column(name = "pro_stock")
+    private String proStock;
+    @Column(name = "pro_status")
+    private String proStatus;
 }
