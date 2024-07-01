c52733e63590d4ad51c6c02072e31d28f2df649d
CRISTHIAN390 <70170324+CRISTHIAN390@users.noreply.github.com>
2024-06-30 19:25:16 -0500

    Mensa

--- src/main/java/com/hb/system/ecommerce/shoes/repositories/ProductRepository.java
+++ src/main/java/com/hb/system/ecommerce/shoes/repositories/ProductRepository.java
@@ -1,21 +1,13 @@
 package com.hb.system.ecommerce.shoes.repositories;
 
-<<<<<<< HEAD
 import com.hb.system.ecommerce.shoes.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import javax.swing.text.html.Option;
 import java.util.List;
-import java.util.Optional;
 
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Integer> {
     List<Product> findByProNameContaining(String proName);
     //Optional<Product> findById(int id);
-=======
-public interface ProductRepository {
-
->>>>>>> criss
 }
