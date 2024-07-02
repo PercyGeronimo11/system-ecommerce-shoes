b487c71b09c14facf4779f5b8d7ef69998c172cd
PercyGeronimo <percy.geronimo.d@gmail.com>
2024-07-01 23:39:00 -0500

    aaaa

--- src/main/java/com/hb/system/ecommerce/shoes/SystemEcommerceShoesAppJavaApplication.java
+++ src/main/java/com/hb/system/ecommerce/shoes/SystemEcommerceShoesAppJavaApplication.java
@@ -4,14 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-//<<<<<<< HEAD
-//import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-//import org.springframework.context.annotation.ComponentScan;
-//
-////@SpringBootApplication
-//@ComponentScan(basePackages = "com.hb.system.ecommerce.shoes")
-//@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-//=======
+
 @ComponentScan(basePackages = "com.hb.system.ecommerce.shoes")
 @SpringBootApplication
 public class SystemEcommerceShoesAppJavaApplication {
