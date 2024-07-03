cdb250fb94cede649ba9a7c262efb9fbe88065d6
alfredo7792 <alfredo7791@gmail.com>
2024-07-02 23:37:01 -0500

    update role

--- src/main/java/com/hb/system/ecommerce/shoes/auth/AuthService.java
+++ src/main/java/com/hb/system/ecommerce/shoes/auth/AuthService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 import com.hb.system.ecommerce.shoes.entity.Role;
 import com.hb.system.ecommerce.shoes.entity.User;
 import com.hb.system.ecommerce.shoes.repositories.UserRepository;
-
+import com.hb.system.ecommerce.shoes.repositories.RolRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -23,6 +23,7 @@ public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private final AuthenticationManager authenticationManager;
+    private final RolRepository roleRepository;
 
     public AuthResponse login(LoginRequest request) {
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
@@ -31,15 +32,17 @@ public class AuthService {
         return AuthResponse.builder()
             .token(jwtService.getToken(userDetails))
             .username(user.getName())
+            .rol(user.getRole().getName())
             .build();
     }
 
     public AuthResponse register(RegisterRequest request) {
+        Role defaultRole = roleRepository.findById(1).orElseThrow(() -> new IllegalArgumentException("Default role not found"));
         User user = User.builder()
             .name(request.getUsername())
             .username(request.getEmail())
             .password(passwordEncoder.encode(request.getPassword()))
-            .role(Role.USER)
+            .role(defaultRole)
             .registerDate(LocalDateTime.now())
             .status(true)
             .build();
