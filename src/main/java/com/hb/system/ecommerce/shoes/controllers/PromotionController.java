01bde445895c43bf0237066af00dec43670793b2
CRISTHIAN390 <70170324+CRISTHIAN390@users.noreply.github.com>
2024-07-07 00:10:05 -0500

    ac ramitacriss

--- src/main/java/com/hb/system/ecommerce/shoes/controllers/PromotionController.java
+++ src/main/java/com/hb/system/ecommerce/shoes/controllers/PromotionController.java
@@ -4,67 +4,101 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.core.io.Resource;
 import com.hb.system.ecommerce.shoes.dto.ApiResponse;
 import com.hb.system.ecommerce.shoes.entity.Promotion;
 import com.hb.system.ecommerce.shoes.services.PromotionService;
-
+import java.io.IOException;
 import java.util.List;
-
+import org.springframework.http.HttpHeaders;
 @RestController
 @CrossOrigin(origins = "*") // Permitir solicitudes desde cualquier origen
 @RequestMapping("/api/promotion")
 public class PromotionController {
     @Autowired
     private PromotionService promotionService;
-
-
     @GetMapping
     public ResponseEntity<ApiResponse<List<Promotion>>> list() {
         List<Promotion> promotions = promotionService.listAll();
-        ApiResponse<List<Promotion>> response= new ApiResponse<>();
+        ApiResponse<List<Promotion>> response = new ApiResponse<>();
         response.setStatus(HttpStatus.OK.value());
         response.setMessage("Lista de promociones exitosamente");
         response.setData(promotions);
-        return new ResponseEntity<>(response,HttpStatus.OK);
-        
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
-    
+
     @PostMapping
-    public ResponseEntity<ApiResponse<Promotion>> create(@RequestBody Promotion promotionRequest){
-        Promotion promocion= promotionService.save(promotionRequest);
-        ApiResponse<Promotion> response= new ApiResponse<>();
+    public ResponseEntity<ApiResponse<Promotion>> create(@RequestPart("promotion") Promotion promotionRequest,
+            @RequestPart("file") MultipartFile file) {
+        Promotion promocion;
+        try {
+            promocion = promotionService.save(promotionRequest, file);
+        } catch (IOException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        } catch (IllegalArgumentException e) {
+            ApiResponse<Promotion> response = new ApiResponse<>();
+            response.setStatus(HttpStatus.BAD_REQUEST.value());
+            response.setMessage(e.getMessage());
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        }
+        ApiResponse<Promotion> response = new ApiResponse<>();
         response.setStatus(HttpStatus.OK.value());
-        response.setMessage("Se registró una promocion exitosamente");
+        response.setMessage("Se registró una promoción exitosamente");
         response.setData(promocion);
-        return new ResponseEntity<>(response,HttpStatus.OK);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<ApiResponse<Promotion>> edit(@PathVariable int id, @RequestBody Promotion promotionRequest){
-        Promotion promocion= promotionService.update(id,promotionRequest);
-        ApiResponse<Promotion> response=new ApiResponse<>();
+    public ResponseEntity<ApiResponse<Promotion>> edit(@PathVariable int id,
+            @RequestPart("promotion") Promotion promotionRequest, @RequestPart("file") MultipartFile file) {
+        Promotion promocion;
+        try {
+            promocion = promotionService.update(id, promotionRequest, file);
+        } catch (IOException e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        } catch (IllegalArgumentException e) {
+            ApiResponse<Promotion> response = new ApiResponse<>();
+            response.setStatus(HttpStatus.BAD_REQUEST.value());
+            response.setMessage(e.getMessage());
+            return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+        }
+        ApiResponse<Promotion> response = new ApiResponse<>();
         response.setStatus(HttpStatus.OK.value());
-        response.setMessage("La promocion se actualizó exitosamente");
+        response.setMessage("La promoción se actualizó exitosamente");
         response.setData(promocion);
-        return new ResponseEntity<>(response,HttpStatus.OK);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
+
     @GetMapping("/{id}")
-    public ResponseEntity<ApiResponse<Promotion>> getById(@PathVariable int id){
-        Promotion promocion= promotionService.getById(id);
-        ApiResponse<Promotion> response=new ApiResponse<>();
+    public ResponseEntity<ApiResponse<Promotion>> getById(@PathVariable int id) {
+        Promotion promocion = promotionService.getById(id);
+        ApiResponse<Promotion> response = new ApiResponse<>();
         response.setStatus(HttpStatus.OK.value());
         response.setMessage("Detalle de la promocion recuperado exitossamente");
         response.setData(promocion);
-        return new ResponseEntity<>(response,HttpStatus.OK);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<ApiResponse<Promotion>> delete(@PathVariable int id){
+    public ResponseEntity<ApiResponse<Promotion>> delete(@PathVariable int id) {
         promotionService.delete(id);
-        ApiResponse<Promotion> response=new ApiResponse<>();
+        ApiResponse<Promotion> response = new ApiResponse<>();
         response.setStatus(HttpStatus.OK.value());
         response.setMessage("Promocion eliminada exitosamente");
-        return new ResponseEntity<>(response,HttpStatus.OK);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/images/{fileName:.+}")
+    public ResponseEntity<Resource> getImage(@PathVariable String fileName) {
+        Resource resource;
+        try {
+            resource = promotionService.loadFileAsResource(fileName);
+        } catch (IOException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
     }
 }
--- src/main/java/com/hb/system/ecommerce/shoes/services/PromotionService.java
+++ src/main/java/com/hb/system/ecommerce/shoes/services/PromotionService.java
@@ -1,9 +1,12 @@
 package com.hb.system.ecommerce.shoes.services;
+
+
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
-
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
 import com.hb.system.ecommerce.shoes.entity.Promotion;
 import com.hb.system.ecommerce.shoes.repositories.PromotionRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,50 +15,50 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class PromotionService {
 
-    // Inyección de dependencias de PromotionRepository
     @Autowired
     private PromotionRepository promotionRepository;
-    
-    // Método para listar todas las promociones activas
+    @Autowired
+    private FileStorageService fileStorageService;
+
     public List<Promotion> listAll() {
-        // Llama al método del repositorio que devuelve todas las promociones activas
         return promotionRepository.findAllActivePromotions();
     }
 
-    // Método para obtener una promoción por su ID
     public Promotion getById(int id) {
-        // Llama al método del repositorio que busca una promoción por su ID
         return promotionRepository.findById(id);
     }
 
-    // Método para guardar una nueva promoción
-    public Promotion save(Promotion resource) {
-        // Establece el estado de la promoción a 'true' (activa)
+    public Promotion save(Promotion resource, MultipartFile file) throws IOException {
+        if (file != null && !file.isEmpty()) {
+            String fileName = fileStorageService.storeFile(file);
+            String fileUrl = "/api/promotion/images/" + fileName;
+            resource.setPromUrlImage(fileUrl);
+        }
         resource.setPromStatus(true);
-        // Guarda la promoción en el repositorio y devuelve el objeto guardado
         return promotionRepository.save(resource);
     }
 
-    // Método para actualizar una promoción existente
-    public Promotion update(int id, Promotion resource){
-        // Comprueba si una promoción con el ID dado existe
+    public Promotion update(int id, Promotion resource, MultipartFile file) throws IOException {
         if (promotionRepository.existsById(id)) {
-            // Establece el ID de la promoción al ID dado
+            if (file != null && !file.isEmpty()) {
+                String fileName = fileStorageService.storeFile(file);
+                String fileUrl = "/api/promotion/images/" + fileName;
+                resource.setPromUrlImage(fileUrl);
+            }
             resource.setId(id);
-            // Guarda la promoción actualizada en el repositorio y devuelve el objeto guardado
             return promotionRepository.save(resource);
-        } else
-            // Si la promoción no existe, devuelve null
-            return null;
+        } else {
+            throw new IllegalArgumentException("Promotion not found with id: " + id);
+        }
     }
 
-    // Método para eliminar (desactivar) una promoción
     public void delete(int id) {
-        // Busca la promoción por su ID
         Promotion promocion = promotionRepository.findById(id);
-        // Establece el estado de la promoción a 'false' (inactiva)
         promocion.setPromStatus(false);
-        // Guarda la promoción con el estado actualizado en el repositorio
         promotionRepository.save(promocion);
+    }
+
+    public Resource loadFileAsResource(String fileName) throws IOException {
+        return fileStorageService.loadFileAsResource(fileName);
     }
 }
